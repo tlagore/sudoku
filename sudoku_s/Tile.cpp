96c6356f3b9f49121b8a9e01e73b25f626b97fb4
tlagore <tyronelagore@gmail.com>
2018-03-28 16:38:41 -0600

    cleaned up Tile a bit

--- sudoku_s/Tile.cpp
+++ sudoku_s/Tile.cpp
@@ -1,8 +1,29 @@
 #include "stdafx.h"
 #include "Tile.h"
 
+//Default Constructors
+Tile::Tile() : actualValue{ -1 }, row{ -1 }, column{ -1 } {}
 
+//Constructor sets value, row and column
+Tile::Tile(int value, int row, int column) : actualValue{ value }, row{ row }, column{ column } {}
+
+//Copy constructor
+Tile::Tile(const Tile &tile) : actualValue{ tile.actualValue }, row{ tile.row }, column{ tile.column } {}
+
+//Override assignment operator
+Tile & Tile::operator=(Tile otherTile)
+{
+	this->actualValue = otherTile.actualValue;
+	this->row = otherTile.row;
+	this->column = otherTile.column;
+	for (auto value : otherTile.possibleValues)
+		this->possibleValues.push_back(value);
+	
+	return *this;
+}
 
 Tile::~Tile()
 {
+
 }
+
