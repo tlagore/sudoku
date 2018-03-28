7e180661f1afd2af19856c7ab33cdf8ebf364bde
JohnnySimmonds <simmondsjohnny@gmail.com>
2018-03-28 16:57:01 -0600

    Wrote board definition. also added some logic for checking if the board is in a valid state

--- sudoku_s/Board.cpp
+++ sudoku_s/Board.cpp
@@ -1,12 +1,72 @@
-#include "stdafx.h"
+ #include "stdafx.h"
 #include "Board.h"
 
 
 Board::Board()
 {
+	for (int row = 0; row < BOARD_SIZE; row++)
+	{
+		for (int column = 0; column < BOARD_SIZE; column++)
+		{
+			board[row][column] = new Tile(INTIAL_VALUE, row, column);
+		}
+	}
 }
-
 
 Board::~Board()
 {
+	for (int row = 0; row < BOARD_SIZE; row++)
+	{
+		for (int column = 0; column < BOARD_SIZE; column++)
+		{
+			delete board[row][column];
+		}
+	}
 }
+
+Tile Board::getTile(int row, int column)
+{
+	return *board[row][column];
+}
+
+void Board::setTileActualValue(int value, int row, int column)
+{
+	board[row][column]->setActualValue(value);
+}
+
+bool Board::isValidState()
+{
+
+	for (int row = 0; row < BOARD_SIZE; row++)
+	{
+		for (int column = 0; column < BOARD_SIZE; column++)
+		{
+			if (!isValidRow(*board[row][column]))
+				return false;
+
+			if (!isValidColumn(*board[row][column]))
+				return false;
+				
+			if (!isValidColumn(*board[row][column]))
+				return false;
+		}
+	}
+	//TODO: IMPLEMENT LOGIC
+	
+}
+
+bool Board::isValidRow(Tile tile)
+{
+	return false;
+}
+
+bool Board::isValidColumn(Tile tile)
+{
+	return false;
+}
+
+bool Board::isValidBox(Tile tile)
+{
+	return false;
+}
+
--- sudoku_s/Board.h
+++ sudoku_s/Board.h
@@ -2,6 +2,7 @@
 
 #include "Tile.h"
 
+#define INTIAL_VALUE -1
 #define BOARD_SIZE 9
 class Board
 {
@@ -9,8 +10,14 @@ class Board
 public:
 	Board();
 	~Board();
+	Tile getTile(int row, int column);
+	void setTileActualValue(int value, int row, int column);
+	bool isValidState();
+	bool isValidRow(Tile tile);
+	bool isValidColumn(Tile tile);
+	bool isValidBox(Tile tile);
 
 private:
-	Tile _aBoard[BOARD_SIZE][BOARD_SIZE];
+	Tile* board[BOARD_SIZE][BOARD_SIZE];
 };
 
